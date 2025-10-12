4d6f01ee418d777f345685932377123852077b41
zziminally <zziminally@gmail.com>
2025-10-13 01:42:45 +0900

    init

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import sys
+
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
